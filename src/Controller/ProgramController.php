513323bbe4574a4b01cbfcfe19d63bdec2570f1d
lucianpurcaru <lucian.purcaru@gmail.com>
2022-11-27 17:14:32 +0100

    first doctrine

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,7 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/program', name: 'program_')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
